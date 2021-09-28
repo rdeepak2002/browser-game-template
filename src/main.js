0f6b8ab99266e53e5627b95655cbf324bd26f6a4
rdeepak2002 <rdeepak2002@gmail.com>
2021-09-28 17:36:46 -0400

    added animation

--- src/main.js
+++ src/main.js
@@ -1,24 +1,41 @@
 // ensure all js is valid
 'use strict';
 
+const SCREEN_WIDTH = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+const SCREEN_HEIGHT = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
 // class to represent the player
 class Player {
   // constructor
   constructor(ctx) {
     this.position = {x: 0, y: 0};
-    this.velocity = {x: 0.1, y: 0.1};
+    this.velocity = {x: 0.3, y: 0.3};
+    this.dimensions = {width: 100, height: 100};
     this.ctx = ctx;
   }
 
   // method to draw the player
   draw(dt) {
-    this.ctx.fillRect(this.position.x, this.position.y, 100, 100);
+    this.ctx.fillRect(this.position.x, this.position.y, this.dimensions.width, this.dimensions.height);
   }
 
   // method to update the player
   update(dt) {
     this.position.x += this.velocity.x * dt;
     this.position.y += this.velocity.y * dt;
+
+    if(this.position.x + this.dimensions.width >= SCREEN_WIDTH || this.position.x <= 0) {
+      this.velocity.x *= -1;
+    }
+
+    if(this.position.y + this.dimensions.height >= SCREEN_HEIGHT || this.position.y <= 0) {
+      this.velocity.y *= -1;
+    }
   }
 }
 
@@ -40,11 +57,10 @@ class Game {
     }
 
     const gameLoop = (timeStamp) => {
+      // note: console.log in the update loop makes the game very laggy
+
       // get time elapsed between frames
       const dt = timeStamp - this.lastUpdatedTime;
-      const fps = 1000 / dt;
-
-      console.log(`fps: ${fps}`);
 
       // clear the screen before drawing over it again
       this.ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -67,6 +83,10 @@ class Game {
   initCanvas() {
     // get the html5 canvas and its context
     this.canvas = document.getElementById('canvas');
+
+    this.canvas.width = SCREEN_WIDTH;
+    this.canvas.height = SCREEN_HEIGHT;
+
     this.ctx = canvas.getContext('2d');
   }
 
